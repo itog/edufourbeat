793e1c2b0c20360b527bae32d68c439bc296d1d2
itog <itog@pigmal.com>
2013-10-26 14:15:48 +0900

    Add start activity

---
+++ src/com/pigmal/android/fourbeat/sample/StartActivity.java
@@ -0,0 +1,26 @@
+package com.pigmal.android.fourbeat.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+}
